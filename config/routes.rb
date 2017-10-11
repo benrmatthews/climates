489e72307628caa5861b3a5467202ae3315bbd82
Ben Matthews <ben@montfort.io>
2017-10-11 14:54:25 +0100

    configured roots

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
   root :to => 'home#index'
 
-  get '/auth/:provider/callback', to: 'sessions#create'
-  get '/auth/failure', to: 'sessions#failure'
+  #get '/auth/:provider/callback', to: 'sessions#create'
+  #get '/home#index', to: 'sessions#failure'
 
 end
